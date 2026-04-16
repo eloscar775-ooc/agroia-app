cf04c606aef1ce8e8efd17abac32905f0072c1ef
Oscar Cuevas <oscar.cuevas7277@alumnos.udg.mx>
2026-04-16 10:10:47 -0400

    🚀 ESTRATEGIA FINAL: Cambio a Java MainActivity y limpieza de plugins

---
+++ android/app/src/main/java/com/agroia/app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.agroia.app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/agroia/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.agroia.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
